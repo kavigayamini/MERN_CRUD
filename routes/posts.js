0e1744291737cc0c473d75cb0bb9b9f8ad416b8a
kavigayamini <cst17025@std.uwu.ac.lk>
2021-10-18 15:22:51 +0530

    API req added

--- routes/posts.js
+++ routes/posts.js
@@ -23,7 +23,54 @@ router.post('/post/save',(req,res) => {
 
 //get posts
 
-  
+  router.get('/posts',(req,res) =>{
+      Posts.find().exec((err,posts) => {
+          if(err){
+              return res.status(400).json({
+                  error:err
+              });
+          }
+          return res.status(200).json({
+              success:true,
+              existingPost:posts
+          });
+      });
+  });
+
+
+  //update posts
+
+  router.put('/post/update/:id',(req,res) => {
+      Posts.findByIdAndUpdate(
+          req.params.id,
+          {
+              $set:req.body
+          },
+          (err,post)=>{
+              if(err){
+                  return res.status(400).json({error:err});
+              }
+
+              return res.status(200).json({
+                  success:"Updated successfully"
+              });
+          }
+      );
+  });
+
+
+  //delete post
+
+  router.delete('/post/delete/:id',(req,res) =>{
+      Posts.findByIdAndRemove(req.params.id).exec((err,deletePost) => {
+          if(err) return res.status(400).json({
+              message:"Delete unsuccessful",err
+          });
+          return res.json({
+              message:"Delete successful",deletePost
+          });
+      });
+  });
 
 module.exports = router;
 
